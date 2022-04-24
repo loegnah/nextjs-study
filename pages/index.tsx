1f1f62258c387a867edc64b8f1495ffad9d31d45
loegnah <loegnah@gmail.com>
2022-04-24 17:28:02 +0900

    Get movie data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,40 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+type MovieData = {
+  id: number;
+  original_title: string;
+};
+
+const API_KEY = "b8c0dbbfd97bd63cddd62402ad180193";
+
 const Home: NextPage = () => {
+  const [movies, setMovies] = useState<MovieData[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title={"Home"} />
-      <h1>Home</h1>
+      {!movies ? (
+        <h4>Loading...</h4>
+      ) : (
+        movies.map((movie) => (
+          <div key={movie.id}>
+            <h4>{movie.original_title}</h4>
+          </div>
+        ))
+      )}
     </div>
   );
 };
