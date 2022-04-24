20c531958441295a7e5837758d106d08f26d30ee
loegnah <loegnah@gmail.com>
2022-04-24 18:04:01 +0900

    Apply rewrites and .env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+const API_KEY = process.env.API_KEY;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movie",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
